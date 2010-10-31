9f97d457f20aa9920f9ad0d3215c5639970820e4
AnonymousCow <anonymouscow@gmail.com>
2010-10-31 09:27:23 +0800

    Initial commit of unmodified new Rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ideatome::Application.config.secret_token = '456fb21ff8433cda78a1ade3413b71b67153e8a6a649ee7e6e700b29de72fa36a383d11e12a44ff9c811527438b5bf20e246752c55da816b06fa0882c97ed8ca'
